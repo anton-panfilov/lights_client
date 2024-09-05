8f57c6b6ebe89fa2d5d589afa0b9598e52e856dc
Anton Panfilov <anton@panfilov.biz>
2024-09-04 20:10:22 -0700

    ESP32 fix persistent storage add arduino nano scheme

--- src/arduino/RGB_Simple_Mofset/lib/WifiCredentials/PersistentStorage/ESP32.cpp
+++ src/arduino/RGB_Simple_Mofset/lib/WifiCredentials/PersistentStorage/ESP32.cpp
@@ -2,20 +2,23 @@
 
 Preferences preferences;
 
-void WiFiCredentials::init() {
-    preferences.begin("wifiCredentials", false);
-}
+void WiFiCredentials::init() {}
 
 void WiFiCredentials::loadFromPersistentStorage() {
+    preferences.begin("wifiCredentials", true);
+
     String stored_ssid = preferences.getString("ssid", "");
     String stored_password = preferences.getString("password", "");
 
     stored_ssid.toCharArray(ssid, maxSSIDLength + 1);
     stored_password.toCharArray(password, maxPasswordLength + 1);
+    preferences.end();
 }
 
 
 void WiFiCredentials::saveToPersistentStorage(char *ssid, char *password) {
+    preferences.begin("wifiCredentials", false);
     preferences.putString("ssid", String(ssid).substring(0, maxSSIDLength));
     preferences.putString("password", String(password).substring(0, maxPasswordLength));
+    preferences.end();
 }
